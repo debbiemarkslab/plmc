1808f0957c0894ecc184edf601ec97c43ce0c50e
Aaron Kollasch <aaron@kollasch.dev>
2023-01-20 18:02:59 -0500

    Reduce protein.sh test precision
    Don't check last decimal digit of couplings output

--- tests/protein.sh
+++ tests/protein.sh
@@ -9,8 +9,12 @@ tmpfile2="$(mktemp /tmp/evcouplings.XXXXXX)"
 echo "$tmpfile2"
 ../bin/plmc -c "$tmpfile" --save-weights "$tmpfile2" -le 16.0 -lh 0.01 -m 100 -g -f DYR_ECOLI ../example/protein/DHFR.a2m
 
+# Compare couplings lines except for last decimal digit
 comp_value=0
-diff --brief "$tmpfile" DHFR.couplings || comp_value=$?
+diff --brief -L "$tmpfile" -L DHFR.couplings \
+    <(while IFS= read -r line; do echo "${line%?}"; done < "$tmpfile") \
+    <(while IFS= read -r line; do echo "${line%?}"; done < DHFR.couplings) ||
+    comp_value=$?
 
 weights_comp=0
 diff --brief "$tmpfile2" DHFR_weights_raw.txt || weights_comp=$?
@@ -41,8 +45,12 @@ tmpfile="$(mktemp /tmp/evcouplings.XXXXXX)"
 tmpfile2="$(mktemp /tmp/evcouplings.XXXXXX)"
 ../bin/plmc -c "$tmpfile" -le 16.0 -lh 0.01 -m 100 -g -f DYR_ECOLI -w DHFR_weights_raw.txt --save-weights "$tmpfile2" ../example/protein/DHFR.a2m
 
+# Compare couplings lines except for last decimal digit
 comp_value=0
-diff --brief "$tmpfile" DHFR.couplings || comp_value=$?
+diff --brief -L "$tmpfile" -L DHFR.couplings \
+    <(while IFS= read -r line; do echo "${line%?}"; done < "$tmpfile") \
+    <(while IFS= read -r line; do echo "${line%?}"; done < DHFR.couplings) ||
+    comp_value=$?
 
 weights_comp=0
 diff --brief "$tmpfile2" DHFR_weights_raw.txt || weights_comp=$?
